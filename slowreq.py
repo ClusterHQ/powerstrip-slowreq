64f200af4093840c71135a4f7943c757a50e94d3
Luke Marsden <luke@clusterhq.com>
2015-01-17 17:23:58 +0000

    Initial commit for powerstrip-slowreq plugin: tests, implementation, readme, requirements and license.

---
+++ slowreq.py
@@ -0,0 +1,35 @@
+# Copyright ClusterHQ Limited. See LICENSE file for details.
+
+from twisted.internet import reactor
+from twisted.internet.task import deferLater
+from twisted.web import server, resource
+import json
+
+class AdapterResource(resource.Resource):
+    isLeaf = True
+    def render_POST(self, request):
+        """
+        Handle a pre-hook.
+        """
+        requestJson = json.loads(request.content.read())
+        if requestJson["Type"] != "pre-hook":
+            raise Exception("unsupported hook type %s" %
+                (requestJson["Type"],))
+        # The desired response is the entire client request
+        # payload, unmodified.
+        def waited():
+            request.write(json.dumps(
+                requestJson["ClientRequest"]))
+            request.finish()
+        deferLater(reactor, 1, waited)
+        return server.NOT_DONE_YET
+
+def getAdapter():
+    root = resource.Resource()
+    root.putChild("slowreq-adapter", AdapterResource())
+    site = server.Site(root)
+    return site
+
+if __name__ == "__main__":
+    reactor.listenTCP(80, getAdapter())
+    reactor.run()
---
+++ test_slowreq.py
@@ -0,0 +1,47 @@
+# Copyright ClusterHQ Limited. See LICENSE file for details.
+
+"""
+Tests for the sample slowreq plugin.
+"""
+
+from treq.client import HTTPClient
+from twisted.internet import reactor
+from twisted.trial.unittest import TestCase
+from twisted.web.client import Agent
+import json
+import treq
+import slowreq
+
+class TestSlowRequests(TestCase):
+    def setUp(self):
+        self.agent = Agent(reactor) # no connectionpool
+        self.client = HTTPClient(self.agent)
+        self.slowreqAPI = slowreq.getAdapter()
+        self.slowreqServer = reactor.listenTCP(0, self.slowreqAPI)
+        self.slowreqPort = self.slowreqServer.getHost().port
+
+    def tearDown(self):
+        return self.slowreqServer.stopListening()
+
+    def test_passthrough_request(self):
+        d = self.client.post('http://127.0.0.1:%d/slowreq-adapter' % (self.slowreqPort,),
+                      json.dumps({
+                          "Type": "pre-hook",
+                          "ClientRequest": {
+                              "Method": "POST",
+                              "Request": "/fictional",
+                              "Body": {"Number": 7}}
+                          }),
+                      headers={'Content-Type': ['application/json']})
+        def verifyResponseCode(response):
+            self.assertEqual(response.code, 200)
+            return response
+        d.addCallback(verifyResponseCode)
+        d.addCallback(treq.json_content)
+        def verify(body):
+            self.assertEqual(body, {
+              "Method": "POST",
+              "Request": "/fictional",
+              "Body": {"Number": 7}})
+        d.addCallback(verify)
+        return d
